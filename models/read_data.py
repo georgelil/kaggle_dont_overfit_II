471bd9bdc8cca27c1fee4962f271fe267fa98b80
George Liljebakk <gliljebakk@gmail.com>
2019-02-16 15:27:45 +0000

    first test run. score on kaggle of 0.669 using a logistic regression

---
+++ models/read_data.py
@@ -0,0 +1,11 @@
+"""
+So as an initial step im going to open up the data
+and have a look around...
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+training_data = pd.read_csv('../data/train.csv')
